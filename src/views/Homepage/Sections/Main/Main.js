9c115a48e84d1562c8d491a8683b595499c1592a
ciurkoj <jakub1798@gmail.com>
2020-08-27 16:09:22 +0100

    added example pictures to main card

--- src/views/Homepage/Sections/Main/Main.js
+++ src/views/Homepage/Sections/Main/Main.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Box, Grid } from "@material-ui/core";
+import { Box, Grid, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import homePageStyles from "../../../../assets/jss/homePageStyles";
+import desing from "../../../../assets/img/design.png";
+import products from "../../../../assets/img/products.jpg";
+import prototype from "../../../../assets/img/prototype.jpg";
+import rdservice from "../../../../assets/img/r&dservice.jpeg";
 const useStyles = makeStyles(homePageStyles);
 
 export default function Main() {
   const classes = useStyles();
   return (
     <div>
-      <Box className={classes.container}>
+      <Box className={classes.container} style={{ color: "black" }}>
         <Grid item md={12}>
           <p
             style={{
@@ -27,6 +31,32 @@ export default function Main() {
             electronics, and control electronics specialists. We offer complete
             solutions from concept through to production.
           </p>
+        </Grid>{" "}
+        <Grid
+          container
+          style={{
+            background: "rgba(0, 0, 0 , 0.2)",
+            display: "inline-flex",
+            borderRadius: "4px",
+          }}
+        >
+          <Grid container justify="center">
+            <Grid item xs={12} style={{ textAlign: "center" }}>
+              <h1>Our services</h1>
+            </Grid>
+            <Grid item xs={3}>
+              <img src={desing} style={{ maxWidth: "200px" }}></img>
+            </Grid>
+            <Grid item xs={3}>
+              <img src={rdservice} style={{ maxWidth: "200px" }}></img>
+            </Grid>
+            <Grid item xs={3}>
+              <img src={prototype} style={{ maxWidth: "200px" }}></img>
+            </Grid>
+            <Grid item xs={3}>
+              <img src={products} style={{ maxWidth: "200px" }}></img>
+            </Grid>
+          </Grid>
         </Grid>
       </Box>
     </div>
