f03a9a57a0a46a7da3e3428a7639756af8062537
ciurkoj <jakub1798@gmail.com>
2020-09-03 23:47:09 +0100

    Updates

---
+++ precache-manifest.37cc3f2310443eb01d87fd31d1e17291.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f218a4e2c9a996328ed1fa2891f84ba4",
+    "url": "/refreshedWebiste/index.html"
+  },
+  {
+    "revision": "326960d6458d88896975",
+    "url": "/refreshedWebiste/static/js/2.34652963.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/refreshedWebiste/static/js/2.34652963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b58425fd0235cd7fd6e",
+    "url": "/refreshedWebiste/static/js/main.e1772dfa.chunk.js"
+  },
+  {
+    "revision": "ac4db2e1ab328947550a",
+    "url": "/refreshedWebiste/static/js/runtime-main.2e056e56.js"
+  },
+  {
+    "revision": "009922cc43347dfd0b8926c34521c8b3",
+    "url": "/refreshedWebiste/static/media/content.009922cc.png"
+  },
+  {
+    "revision": "cff7e9cb8f5c7909820ab49cfcc809cc",
+    "url": "/refreshedWebiste/static/media/design-icon.cff7e9cb.png"
+  },
+  {
+    "revision": "05a002a47c2a27571db215e796789076",
+    "url": "/refreshedWebiste/static/media/design.05a002a4.png"
+  },
+  {
+    "revision": "54159639693adaf03085a023971d076f",
+    "url": "/refreshedWebiste/static/media/lyra-electronics.54159639.png"
+  },
+  {
+    "revision": "465ce390554e9fbbeb58ec85d572896f",
+    "url": "/refreshedWebiste/static/media/lyraWhiteLogo.465ce390.png"
+  },
+  {
+    "revision": "cd8fdb9a9bee25424f831b346d4e13cd",
+    "url": "/refreshedWebiste/static/media/pcb.cd8fdb9a.png"
+  },
+  {
+    "revision": "0478da71a6ab0bc57ee2d8826137d1a9",
+    "url": "/refreshedWebiste/static/media/product-icon.0478da71.png"
+  },
+  {
+    "revision": "eec7f364fb0edcf4c2d0e55eefc7b488",
+    "url": "/refreshedWebiste/static/media/products.eec7f364.png"
+  },
+  {
+    "revision": "7df01eea6998c1be2f1cfea8ab253288",
+    "url": "/refreshedWebiste/static/media/prototype-icon.7df01eea.svg"
+  },
+  {
+    "revision": "3c40a204697b2237695f8613d10196ce",
+    "url": "/refreshedWebiste/static/media/prototype.3c40a204.png"
+  },
+  {
+    "revision": "10377fdd58701799611a58d58f53b366",
+    "url": "/refreshedWebiste/static/media/r&dservice.10377fdd.jpeg"
+  },
+  {
+    "revision": "03b2c3404cc739f2a58115d690546282",
+    "url": "/refreshedWebiste/static/media/research-icon.03b2c340.png"
+  }
+]);
